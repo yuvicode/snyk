c1e39748cbd3652abb6f94ce4ef5476ea0320ade
ghe <lili@snyk.io>
2020-06-22 15:26:22 +0100

    wip

--- src/cli/commands/ignore.ts
+++ src/cli/commands/ignore.ts
@@ -1,6 +1,6 @@
 export = ignore;
 
-import * as policy from 'snyk-policy';
+import * as policyLib from 'snyk-policy';
 import chalk from 'chalk';
 import * as authorization from '../../lib/authorization';
 import * as auth from './auth/is-authed';
@@ -13,66 +13,86 @@ const debug = Debug('snyk');
 
 import { MisconfiguredAuthInCI } from '../../lib/errors/misconfigured-auth-in-ci-error';
 
-function ignore(options): Promise<MethodResult> {
+interface IgnoreOptions {
+  reason?: string;
+  id: string;
+  expiry: string | Date;
+}
+
+async function ignore(options: IgnoreOptions): Promise<MethodResult> {
   debug('snyk ignore called with options: %O', options);
 
-  return auth
-    .isAuthed()
-    .then((authed) => {
-      if (!authed) {
-        if (isCI()) {
-          throw MisconfiguredAuthInCI();
-        }
-      }
-      apiTokenExists();
-    })
-    .then(() => {
-      return authorization.actionAllowed('cliIgnore', options);
-    })
-    .then((cliIgnoreAuthorization) => {
-      if (!cliIgnoreAuthorization.allowed) {
-        debug('snyk ignore called when disallowed');
-        console.log(chalk.bold.red(cliIgnoreAuthorization.reason));
-        return;
-      }
+  const authed = await auth.isAuthed();
+  if (!authed && isCI()) {
+    throw MisconfiguredAuthInCI();
+  }
 
-      if (!options.id) {
-        throw Error('idRequired');
-      }
-      options.expiry = new Date(options.expiry);
-      if (options.expiry.getTime() !== options.expiry.getTime()) {
-        debug('No/invalid expiry given, using the default 30 days');
-        options.expiry = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
-      }
-      if (!options.reason) {
-        options.reason = 'None Given';
-      }
+  await apiTokenExists();
+  const cliIgnoreAuthorization = await authorization.actionAllowed(
+    'cliIgnore',
+    options,
+  );
+  if (!cliIgnoreAuthorization.allowed) {
+    debug('snyk ignore called when disallowed');
+    console.log(chalk.bold.red(cliIgnoreAuthorization.reason));
+    return;
+  }
+  if (!options.id) {
+    throw Error('idRequired');
+  }
+  options.expiry = new Date(options.expiry);
+  if (options.expiry.getTime() !== options.expiry.getTime()) {
+    debug('No/invalid expiry given, using the default 30 days');
+    options.expiry = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
+  }
+  if (!options.reason) {
+    options.reason = 'None Given';
+  }
 
-      debug(
-        'changing policy: ignore "%s", for all paths, reason: "%s", until: %o',
-        options.id,
-        options.reason,
-        options.expiry,
-      );
-      return policy
-        .load(options['policy-path'])
-        .catch((error) => {
-          if (error.code === 'ENOENT') {
-            // file does not exist - create it
-            return policy.create();
-          }
-          throw Error('policyFile');
-        })
-        .then(async function ignoreIssue(pol) {
-          pol.ignore[options.id] = [
-            {
-              '*': {
-                reason: options.reason,
-                expires: options.expiry,
-              },
-            },
-          ];
-          return await policy.save(pol, options['policy-path']);
-        });
-    });
+  debug(
+    'changing policy: ignore "%s", for all paths, reason: "%s", until: %o',
+    options.id,
+    options.reason,
+    options.expiry,
+  );
+
+  let policy: PolicyObject;
+  try {
+    policy = await policyLib.load(options['policy-path']);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      // file does not exist - create it
+      policy = policyLib.create();
+    }
+    throw Error('policyFile');
+  }
+  const updatedPolicy = await ignoreIssue(options, policy);
+  return updatedPolicy;
+}
+
+async function ignoreIssue(options: IgnoreOptions, pol: PolicyObject) {
+  pol.ignore[options.id] = [
+    {
+      '*': {
+        reason: options.reason,
+        expires: options.expiry,
+      },
+    },
+  ];
+  return await policyLib.save(pol, options['policy-path']);
+}
+
+interface PolicyObject {
+  skipVerifyPatch?: boolean;
+  ignore: { [issueId: string]: PolicyRule[] };
+  patch: { [issueId: string]: PolicyRule[] };
+  __filename?: string;
+}
+interface PolicyRule {
+  [path: string]: RuleOptions;
+}
+interface RuleOptions {
+  disregardIfFixable?: true;
+  expires?: string | Date;
+  reason?: string;
 }
