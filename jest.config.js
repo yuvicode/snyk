ae96bfd28bc9a8013cf914d00eaa2b5d150ec585
Jakub Mikulas <jakub@snyk.io>
2021-03-01 17:46:58 +0100

    test: go brrr

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
   transform: {}, // ignore .babelrc file
   collectCoverage: false, // not collecting coverage for now
   collectCoverageFrom: ['src/**/*.ts'],
-  coverageReporters: ['text-summary', 'html'],
   testMatch: [
     '<rootDir>/test/*.spec.ts',
     '<rootDir>\\test\\*.spec.ts', // for Windows
@@ -12,6 +11,6 @@ module.exports = {
   modulePathIgnorePatterns: [
     '<rootDir>/test/.*fixtures',
     '<rootDir>/test/acceptance/*',
-    '<rootDir>/test/acceptance/workspaces/*' // to avoid `jest-haste-map: Haste module naming collision` errors
+    '<rootDir>/test/acceptance/workspaces/*', // to avoid `jest-haste-map: Haste module naming collision` errors
   ],
 };
