d7a4d38ff6418eccec835c343e383ae884252b10
ghe <lili@snyk.io>
2021-03-03 22:02:38 +0000

    feat: formatting fixes & tests

--- packages/snyk-fix/test/functional/lib/output-formatters.ts/format-unresolved-item.spec.ts
+++ packages/snyk-fix/test/functional/lib/output-formatters.ts/format-unresolved-item.spec.ts
@@ -1,1 +1,14 @@
-format-unresolved-item
+import { formatUnresolved } from '../../../../src/lib/output-formatters/format-unresolved-item';
+import { generateEntityToFix } from '../../../helpers/generate-entity-to-fix';
+
+describe('format unresolved item', () => {
+  it('successful item & changes formatted', async () => {
+    const entity = generateEntityToFix(
+      'pip',
+      'requirements.txt',
+      JSON.stringify({}),
+    );
+    const res = await formatUnresolved(entity, 'Failed to process item');
+    expect(res).toMatchSnapshot();
+  });
+});
